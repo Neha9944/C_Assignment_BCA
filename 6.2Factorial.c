4a786c51d995acaaa836c3cc4a32c7ca796ef6ab
Niharika Muzumdar <160496880+Neha9944@users.noreply.github.com>
2024-10-26 22:29:29 +0530

    6.2Factorial.c

--- 6.2Factorial.c
+++ 6.2Factorial.c
@@ -6,13 +6,14 @@ int main() {
    printf("Enter a positive integer: ");
    scanf("%d",&number);
    if(number>=0){
+       
+     do{
+         fact =fact*i;
+         i++;
+     }
 
-  while(i<=number)
- {
-    fact =fact*i;
-    i++;
-
- }
+  while(i<=number);
+ 
 
 printf("Factorial of %d is %d",number,fact);
 }else {
