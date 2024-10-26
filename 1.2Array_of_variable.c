ea2581cf8c1f91d7d7fc407f3e405896c1e39c6c
Niharika Muzumdar <160496880+Neha9944@users.noreply.github.com>
2024-10-26 22:45:18 +0530

    Update 1.2Array_of_variable.c

--- 1.2Array_of_variable.c
+++ 1.2Array_of_variable.c
@@ -12,12 +12,14 @@ int main()
     //Get all elements using for loop and store it in array
     printf("Enter %d elements: ",size);
     for(i = 0; i < size; i++)
+    {
           scanf("%d",&arr[i]);
-          
+    }
     //add all elements to the variable sum.
     for(i = 0; i < size; i++)
+    {
           sum = sum + arr[i]; // same as sum += arr[i];
-    
+    }
     //print the result
     printf("Sum of the elements: %d",sum);
     
